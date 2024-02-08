7620069488938d271f323d125f4d8def3cfc712d
Amaan <130492547+amaansodagar786@users.noreply.github.com>
2024-02-08 22:16:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function openmenu() {
+
+    if (document.getElementById('hlo').style.display !== "none") {
+        document.getElementById('hlo').style.display = "none"
+    }
+    else {
+        document.getElementById('hlo').style.display = "block"
+
+    }
+}
